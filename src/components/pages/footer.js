409c7f44155dc949034ca457087870c1728e0fa6
Kory <koriellopez03@gmail.com>
2024-10-19 18:46:05 -0500

    first draft

---
+++ src/components/pages/footer.js
@@ -0,0 +1,66 @@
+function Footer() {
+  return (
+    <div
+      className="d-flex justify-content-evenly text-white position-absolute bottom-0 w-100"
+      style={{ backgroundColor: "#332219", height: "10%" }}
+    >
+      <img src="The_Brew_Logo.png" alt="" />
+      <div>
+        <div className="col">
+          <a
+            href="/"
+            className="row link-underline link-underline-opacity-0 text-white"
+          >
+            Menu
+          </a>
+          <a
+            href="/"
+            className="row link-underline link-underline-opacity-0 text-white"
+          >
+            Location
+          </a>
+        </div>
+      </div>
+      <div>
+        <div className="col">
+          <a
+            href="/"
+            className="row link-underline link-underline-opacity-0 text-white"
+          >
+            Social Media
+          </a>
+          <a
+            href="https://www.instagram.com"
+            className="row link-underline link-underline-opacity-0 text-white"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.facebook.com"
+            className="row link-underline link-underline-opacity-0 text-white"
+          >
+            Facebook
+          </a>
+        </div>
+      </div>
+      <div>
+        <div className="col">
+          <a
+            href="/"
+            className="row link-underline link-underline-opacity-0 text-white"
+          >
+            Contact Us
+          </a>
+          <p href="/" className="row my-0">
+            the.brew@email.com
+          </p>
+          <p href="/" className="row my-0">
+            +1 956-123-4567
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
