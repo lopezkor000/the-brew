e45dcc0d474ec1901253e0e9cf8de7e9e30d800b
Roy Palacios <punc4k3@Roys-MacBook-Air.local>
2024-10-20 06:22:05 -0500

    working link to menu

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Menu />} />
+        <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/seasonal-drinks" element={<SeasonalDrinks />} />
